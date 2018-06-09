ffa6465b9a74a26d9662c4a5e0002924412e0f51
JuanDi <juandixlsims@gmail.com>
2018-06-09 11:49:16 +0200

    :hankey: Enabled ToDo selection (to improve)

--- src/modules/Todos/Todos.jsx
+++ src/modules/Todos/Todos.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { TodoList } from './TodoList';
 import { TodoForm } from './TodoForm';
 
-export const Todos = ({ todos, newTodoValue, actions }) => (
+export const Todos = ({ todos, newTodoValue, selectedTodo, actions }) => (
   <div className="todo-list">
     <h1>todos</h1>
     <p>{todos.length} remaining</p>
-    <TodoList todos={todos} actions={actions} />
-    <TodoForm newTodoValue={newTodoValue} actions={actions} />
+    <TodoList todos={todos} selectedTodo={selectedTodo} actions={actions} />
+    <TodoForm
+      newTodoValue={newTodoValue}
+      selectedTodo={selectedTodo}
+      actions={actions}
+    />
   </div>
 );
